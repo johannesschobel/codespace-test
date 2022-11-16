c741ef2824a97254b79d6f1a81a125f43bb19360
johannesschobel <johannes.schobel@googlemail.com>
2022-11-16 11:33:48 +0100

    Initial commit

---
+++ apps/codespace-web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'codespaces-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'codespace-web';
+}
